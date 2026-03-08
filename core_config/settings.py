3e437998dabf5f82ddfdd36093339efe87ec0c46
sagar n mutalik <sagarnmutalik6@gmailcom>
2026-03-08 18:04:44 +0530

    minor updates

--- core_config/settings.py
+++ core_config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!86uk%!d@nex(c6h4io8fspv^rx6w3os3#$(p_69r=k(km$1w+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
